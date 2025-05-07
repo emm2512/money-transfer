34ca9413de8dcf3c51c726e1a2e8da72894ba9ec
emm2512 <emmamound2@gmail.com>
2025-05-07 03:18:01 +0100

    moneyT

---
+++ app.py/second.py
@@ -0,0 +1,26 @@
+age = 34
+f_name = "serge"
+student_salary = 20000
+is_student = False
+is_online = True
+
+print("your age is",age, "and your name is",f_name)
+print(f"hello {f_name}, your age is {age}, your salary is {salary:,} ")
+print(is_student)
+
+# invalid variable
+""" 
+2name
+keywords = if, for , import,  int, def , while
+first-name
+"""
+print(2+2)
+
+# Data type
+"""
+number, string, bolean, list, tuple, set, dict
+number:
+- integer
+- float (decimal points)
+- complex with imaginary
+"""
